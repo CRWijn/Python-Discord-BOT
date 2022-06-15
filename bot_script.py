ee300aa6f88946c2dc3e1c75a9a8a95a70f48638
crwijnveen <c.r.wijnveen@student.tudelft.nl>
2022-06-15 07:23:20 +0200

    Added steam free games bot
    Used selenium to find free-to-keep games on steam. Still need to check if the epic games bot switches games and pings automatically but that will come tomorrow when the games switch. Also changed the tick interval to 12 hours (43200 seconds)

--- bot_script.py
+++ bot_script.py
@@ -6,6 +6,16 @@ import epicstore_api
 
 from dotenv import load_dotenv, find_dotenv
 from discord.ext import commands
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.service import Service as ChromeService
+from subprocess import CREATE_NO_WINDOW
 
 
 load_dotenv('token.env')
@@ -21,7 +31,9 @@ global timer, stop_timer
 async def on_ready():
     print("Bot initialised")
     channel = bot.get_channel(FREE_GAME_CHANNEL_ID)
+    test_channel = bot.get_channel(TEST_CHANNEL_ID)
     while (True):
+        print("Doing EPIC STORE Check")
         EGS_obj = epicstore_api.api.EpicGamesStoreAPI()
         free_games = EGS_obj.get_free_games()
 
@@ -50,24 +62,123 @@ async def on_ready():
                         with open('free_games.txt', 'w') as f:
                             f.write(game['title'] + "\n")
                             f.close()
-                else:
-                    try:
-                        found = False
-                        with open('free_games.txt') as f:
-                            fdata = f.read()
-                            for line in f:
-                                if line == game['title'] + "\n":
-                                    found = True
-                            f.close()
-                        if found:
-                            fdata.replace(game['title'] + "\n", "")
-                            with open('free_games.txt', 'w') as f:
-                                f.write(fdata)
-                                f.close()
-                    except FileNotFoundError:
+                        await channel.send("<@&867045278694637578>" + " " + game['title'] + " is free to keep on the epic store!")
+                        if "\home" in game['productSlug']:
+                            slug = game['productSlug'].split("\home")[0]
+                            await channel.send("https://store.epicgames.com/en-US/p/" + slug)
+                        else:
+                            await channel.send("https://store.epicgames.com/en-US/p/" + game['productSlug'])
+            else:
+                try:
+                    found = False
+                    with open('free_games.txt') as f:
+                        fdata = f.read()
+                        f.close()
+                    with open('free_games.txt') as f:
+                        for line in f:
+                            if line == game['title'] + "\n":
+                                found = True
+                        f.close()
+                    if found:
+                        fdata = fdata.replace(game['title'] + "\n", "")
                         with open('free_games.txt', 'w') as f:
+                            f.write(fdata)
                             f.close()
-        await asyncio.sleep(10)
+                except FileNotFoundError:
+                    with open('free_games.txt', 'w') as f:
+                        f.close()
+
+        print("Beginning Steam Check")
+        chromeOptions = Options()
+        chromeOptions.headless = True
+        chrome_service = ChromeService('chromedriver')
+        chrome_service.creationflags = CREATE_NO_WINDOW
+
+        browser = webdriver.Chrome(options = chromeOptions, service = chrome_service)
+        browser.get('https://store.steampowered.com/search/?maxprice=free&specials=1')
+
+        try:
+            game_names_list = []
+            main = WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.ID, 'search_resultsRows')))
+
+            games = main.find_elements(by=By.TAG_NAME, value="a")
+            for i, game in enumerate(games):
+                print(i)
+                link = game.get_attribute('href')
+                new_tab = webdriver.Chrome(options = chromeOptions)
+                new_tab.get(link)
+                try:
+                    year_entry = new_tab.find_element(by=By.ID, value='ageYear')
+                    year_entry_select = Select(year_entry)
+                    year_entry_select.select_by_value('2000')
+                    new_tab.find_element(by=By.ID, value='view_product_page_btn').click()
+                except NoSuchElementException:
+                    pass
+                game_purchase = WebDriverWait(new_tab, 10).until(EC.presence_of_element_located((By.ID, 'game_area_purchase')))
+                try:
+                    dlc_bub = game_purchase.find_element(by=By.CLASS_NAME, value='game_area_dlc_bubble ')
+                except NoSuchElementException:
+                    try:
+                        disc_block = game_purchase.find_element(by=By.CLASS_NAME, value='game_purchase_discount')
+                        discount_percent = disc_block.find_element(by=By.CLASS_NAME, value='discount_pct')
+                        if discount_percent.text == "-100%":
+                            game_names_list.append((link, new_tab.find_element(by=By.ID, value='appHubAppName').text))
+                    except NoSuchElementException:
+                        pass
+                finally:
+                    new_tab.quit()
+
+            literal_names = []
+            for game in game_names_list:
+                game_name = game[1]
+                literal_names.append(game_name + "\n")
+                try:
+                    found = False
+                    with open('free_games_steam.txt') as f:
+                        for line in f:
+                            if line == game_name + "\n":
+                                found = True
+                        f.close()
+                    if not found:
+                        with open('free_games_steam.txt', 'a') as f:
+                            f.write(game_name + "\n")
+                            f.close()
+                        #ANNOUNCEMENT
+                        await channel.send("<@&867045278694637578>" + " " + game_name + " is free to keep on the steam store!")
+                        await channel.send(game[0])
+                except FileNotFoundError:
+                    with open('free_games_steam.txt', 'w') as f:
+                        f.write(game_name + "\n")
+                        f.close()
+                    await channel.send("<@&867045278694637578>" + " " + game_name + " is free to keep on the steam store!")
+                    await channel.send(game[0])
+
+            removal_list = []
+            with open('free_games_steam.txt') as f:
+                sdata = f.read()
+                f.close()
+            with open('free_games_steam.txt') as f:
+                for line in f:
+                    if line not in literal_names:
+                        removal_list.append(line)
+                f.close()
+            for game_to_remove in removal_list:
+                sdata = sdata.replace(game_to_remove, "")
+                with open('free_games_steam.txt', 'w') as f:
+                    f.write(sdata)
+                    f.close()
+            
+        except Exception as e:
+            print(e)
+            browser.quit()
+            try:
+                new_tab.quit()
+            except:
+                pass
+        finally:
+            browser.quit()
+
+        await asyncio.sleep(43200)
 
 @bot.command(
     name='print',
